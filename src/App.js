18eb4d259c0cbeb5cc66b2a289ad16b38f5ff0bf
cabrerajesus <jesus.cabrera@capital-minds.com>
2023-10-28 19:23:15 -0400

    Agregando componentes y optimizando la plantilla

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import AppHeader from './components/header';
 import AppHero from './components/hero';
+import AppAbout from './components/about';
+import AppWorks from './components/works';
+import AppTeams from './components/teams';
+import AppTestimonials from './components/testimonials';
+import AppPricing from './components/pricin';
+import AppBlog from './components/blog';
 
 function App() {
   return (
@@ -11,6 +17,13 @@ function App() {
       </header>
       <main>
         <AppHero/>
+        <AppAbout/>
+        <AppWorks/>
+        <AppTeams/>
+        <AppTestimonials/>
+        <AppPricing/>
+        <AppBlog/>
+
       </main>
     </div>
   );
